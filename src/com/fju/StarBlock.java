e58351db8a8dd1c7430d9d99a0b534c1bdd9870f
Oscar Ho <oscar.ho0210@gmail.com>
2021-11-27 17:13:33 +0800

    midexam p2 change to while and switch case

--- src/com/fju/StarBlock.java
+++ src/com/fju/StarBlock.java
@@ -9,17 +9,37 @@ public class StarBlock {
         int firstNumber = scanner.nextInt();
         System.out.println("Please enter second number: ");
         int secondNumber = scanner.nextInt();
+        int i = 1;
+        while (i < 8){
+            int j = 1;
+            while (j < 20){
+                switch (j){
+                    case 3:
+                    case 4:
+                    case 5:
+                        System.out.print("*");
+                        break;
+                    default:
+                        System.out.print("0");
+                        break;
+                }
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
 
-        for (int i = 0; i<8; i++){
+
+        /*for (int i = 0; i<8; i++){
             for (int j = 0; j<20; j++){
-                if (j >= firstNumber-1 && j <= secondNumber-1){
+                if (firstNumber-1 <= j && j <= secondNumber-1){
                     System.out.print("*");
                 }else {
                     System.out.print("0");
                 }
             }
             System.out.println();
-        }
+        }*/
 
     }
 }
